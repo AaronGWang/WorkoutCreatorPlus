3baf6a13fbeb3759c5c5bc9b4a2888fa3fbfde90
Aaron Wang <aarongwang2008@gmail.com>
2024-03-13 11:58:19 +0800

    created generate_lower_split

--- helper_functions.py
+++ helper_functions.py
@@ -22,44 +22,24 @@ def create_unique_dataframes(df: pd.DataFrame):
   return dataframes
 
 
-def choose_arms_exercises(area_1_dataframe: pd.DataFrame, arms_dataframe_list: list):
+def choose_exercise(area_df: pd.DataFrame, area_df_list: list):
   '''
   This function chooses random exercises from the arms dataframe.
   
   Args:
-    area_1_dataframe (pd.DataFrame): The dataframe for the first area.
-    arms_dataframe_list (list): A list of dataframes for the arms area.
+    area_df (pd.DataFrame): The dataframe for the area.
+    arms_df_list (list): A list of dataframes for the arms area.
 
   Returns:
-    list: A list of exercises for the arms (each has "Index", "Name", "Area", "Weight", "Rep_num", "RIR", and "Instructions").
+    list: A list of exercises for the area (each has "Index", "Name", "Area", "Weight", "Rep_num", "RIR", and "Instructions").
   '''
-  arm_exercises = []
+  exercises = []
 
-  for i in range(len(arms_dataframe_list)):
-    exercise = area_1_dataframe.loc[random.choice(list(arms_dataframe_list[i]['Index'])) - 1]
-    arm_exercises.append(exercise)
+  for i in range(len(area_df_list)):
+    exercise = area_df.loc[random.choice(list(area_df_list[i]['Index'])) - 1]
+    exercises.append(exercise)
 
-  return arm_exercises
-
-
-def choose_push_pull_exercises(area_2_dataframe: pd.DataFrame, push_pull_dataframe_list: list):
-  '''
-  This function chooses random exercises from the push/pull dataframe.
-
-  Args:
-    area_2_dataframe (pd.DataFrame): The dataframe for the second area.
-    push_pull_dataframe_list (list): A list of dataframes for the push/pull area.
-
-  Returns:
-    list: A list of exercises for the push/pull (each has "Index", "Name", "Area", "Weight", "Rep_num", "RIR", and "Instructions").
-  '''
-  push_pull_exercises = []
-
-  for i in range(len(push_pull_dataframe_list)):
-    exercise = area_2_dataframe.loc[random.choice(list(push_pull_dataframe_list[i]['Index'])) - 1]
-    push_pull_exercises.append(exercise)
-
-  return push_pull_exercises
+  return exercises
 
 
 def generate_upper_split(area_1_dataframe: pd.DataFrame,
@@ -82,9 +62,11 @@ def generate_upper_split(area_1_dataframe: pd.DataFrame,
   arms_dataframe_list = create_unique_dataframes(df=area_1_dataframe)
   push_pull_dataframe_list = create_unique_dataframes(df=area_2_dataframe)
 
-  arm_exercises = choose_arms_exercises(area_1_dataframe=area_1_dataframe, arms_dataframe_list=arms_dataframe_list)
+  arm_exercises = choose_exercise(area_df=area_1_dataframe,
+                                        area_df_list=arms_dataframe_list)
 
-  push_pull_exercises = choose_push_pull_exercises(area_2_dataframe=area_2_dataframe, push_pull_dataframe_list=push_pull_dataframe_list)
+  push_pull_exercises = choose_exercise(area_df=area_2_dataframe,
+                                              area_df_list=push_pull_dataframe_list)
 
   compound_upper_exercises = compound_dataframe.loc[compound_dataframe['Area'] == 'Upper']
   compound_exercise = compound_dataframe.loc[random.choice(list(compound_upper_exercises['Index'])) - 1]
@@ -95,7 +77,30 @@ def generate_upper_split(area_1_dataframe: pd.DataFrame,
   upper_split.append(compound_exercise)
 
   return upper_split
-  
 
 
-  
+def generate_lower_split(area_3_dataframe: pd.DataFrame, compound_dataframe: pd.DataFrame):
+  '''
+  This function generates the lower split section of the workout.
+
+  Args:
+    area_3_dataframe (pd.DataFrame): A dataframe for the third area.
+    compound_dataframe (pd.DataFrame): A dataframe for the compound exercises.
+
+  Returns:
+    list: A list of exercises for the lower split workout.
+  '''
+
+  lower_split = []
+
+  leg_dataframe_list = create_unique_dataframes(df=area_3_dataframe)
+  leg_exercises = choose_exercise(area_df=area_3_dataframe,
+                                  area_df_list=leg_dataframe_list)
+
+  compound_upper_exercises = compound_dataframe.loc[compound_dataframe['Area'] == 'Lower']
+  compound_exercise = compound_dataframe.loc[random.choice(list(compound_upper_exercises['Index'])) - 1]
+
+  lower_split = leg_exercises.copy()
+  lower_split.append(compound_exercise)
+
+  return lower_split
